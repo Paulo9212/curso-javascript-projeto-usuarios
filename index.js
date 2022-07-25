0d3189b181d95e7f99292c13232dd62d9d9e8631
Paulo_Rb <papalohp7@gmail.com>
2022-07-25 18:06:17 -0300

    25/07

---
+++ index.js
@@ -0,0 +1,27 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+
+    if(field.name == 'gender'){
+
+        if(field.checked){
+            console.log("Sim",field)
+        }
+        
+    }else{
+        console.log("Não")
+    }
+
+    console.log(field.id ,field.name, field.value, field.checked, index );
+
+});
